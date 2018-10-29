f16297c622b7d65b16d7a994d8ac86493bf5a9bc
Ignacio Alonso <nacho.allibre@gmail.com>
2018-10-29 17:09:14 -0600

    added new routes for spaces and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: %i[show] do
+    resources :spaces, only: %i[create]
+  end
+
   resources :spaces, except: %i[new destroy] do
     resources :rentals, only: %i[create]
   end
-  resources :spaces, only: %i[filter] do
-  end
+  resources :spaces, only: %i[filter]
 end
