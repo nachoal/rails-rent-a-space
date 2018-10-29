08e96d79fdb76508aa8af16676bcee9548c69bb2
Ignacio Alonso <nacho.allibre@gmail.com>
2018-10-29 17:17:22 -0600

    added a root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root 'spaces#index' 
   resources :users, only: %i[show] do
     resources :spaces, only: %i[create]
   end
