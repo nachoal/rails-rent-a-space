41265b2ef041f7bbe693d4e69b08b3511f2499c6
Ignacio Alonso <nacho.allibre@gmail.com>
2018-10-29 15:38:01 -0600

    added routes for our project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :spaces, except: %i[new destroy] do
+    resources :rentals, only: %i[create]
+  end
+  resources :spaces, only: %i[filter] do
+  end
 end
