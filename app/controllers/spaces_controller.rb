942e147def1c4b951c6fb0afe93d866d0f34f4d7
Ignacio Alonso <nacho.allibre@gmail.com>
2018-10-30 12:44:25 -0600

    finished the Provider form to add a new place for rent

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -8,9 +8,20 @@ class SpacesController < ApplicationController
     @space = Space.find(params[:id])
   end
 
+  def create
+    @user = User.find(params[:user_id])
+    @space = @user.spaces.new(space_params)
+    if @space.save
+      redirect_to @user, notice: 'Your space was succesfully created!'
+    else
+      render @user
+    end
+  end
+
   private
 
   def space_params
-  params.require(:space).permit(:address, :city, :country, :capacity, :description, :price, :photo)
+    params.require(:space).permit(:address, :city, :country,
+                                  :capacity, :description, :price, :photo)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @spaces = @user.spaces
+    @space = @user.spaces.new
+  end
+
+  private
+
+  def space_params
+    params.require(:space).permit(:address, :city, :country,
+                                 :capacity, :description, :price, :photo)
   end
 end
