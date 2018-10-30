f570c7fd6a147d76fbde8315837268525c2a1b3e
Guillaume Balas <guillaume.balas@gmail.com>
2018-10-30 12:21:32 -0600

    Addeed Seed for creatign 6 spaces

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 require 'faker'
 
-5.times do
-  User.new(email: Faker::Internet.email, password: 'testtest')
+6.times do
+  Space.new(name: Faker::Company.buzzword,
+            description: Faker::Lorem.paragraph,
+            address: Faker::Address.street_address,
+            city: Faker::Address.city,
+            country: Faker::Address.country,
+            capacity: [25..1000].sample,
+            price: [2000..400.000].sample,
+            user_id: [1..2].sample)
 end
+
+puts "6 space created successfully!"
