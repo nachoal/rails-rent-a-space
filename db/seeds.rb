6f67842d35d9c72e454ff338588fdafacb2ef721
SOphia Handtmann <sophiahandtmann@gmx.net>
2018-10-31 12:55:39 -0600

    Current status of spaces show page + rentals controller + modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,22 @@
-require 'faker'
+Space.destroy_all
 
-6.times do
-  space = Space.new(name: Faker::Company.buzzword,
-            description: Faker::Lorem.paragraph,
-            address: Faker::Address.street_address,
-            city: Faker::Address.city,
-            country: Faker::Address.country,
-            capacity: [25..1000].sample,
-            price: [2000..400.000].sample,
-            user_id: 1)
+executions = 6
+executions.times do
+  space = Space.new(
+    name: Faker::Company.buzzword,
+    description: Faker::Lorem.paragraph,
+    address: Faker::Address.street_address,
+    city: Faker::Address.city,
+    country: Faker::Address.country,
+    capacity: (25..1000).to_a.sample,
+    price: (2000..400_000).to_a.sample,
+    user: User.first,
+    wifi: true,
+    bathrooms: 2,
+    cleaning: false
+
+  )
   space.save!
 end
 
-puts "6 space created successfully!"
+puts "#{executions} space created successfully!"
