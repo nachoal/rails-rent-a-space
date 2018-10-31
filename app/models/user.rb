1db502372a38548f3a2ef26cf7c5e8201bcadf09
Guillaume Balas <guillaume.balas@gmail.com>
2018-10-31 13:14:37 -0600

    User signup form done with devise + simple_form + bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,9 @@ class User < ApplicationRecord
 
   mount_uploader :photo, PhotoUploader
 
+  before_create :photo_or_default
+
   def photo_or_default
-    photo.file.nil? ? "https://res.cloudinary.com/oulrij/image/upload/v1540927173/auczgzyhap7oluqylboo.jpg" : photo
+    @photo = photo.file.nil? ? "https://res.cloudinary.com/oulrij/image/upload/v1540927173/auczgzyhap7oluqylboo.jpg" : photo
   end
 end
