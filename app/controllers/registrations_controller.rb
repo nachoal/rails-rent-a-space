e2f7bb17596757ac8c9b362020ddcc74fb1e81ae
Guillaume Balas <guillaume.balas@gmail.com>
2018-11-02 11:22:36 -0600

    Saving change to GMap JS modification making available Map object in the window las

--- app/controllers/registrations_controller.rb
+++
@@ -1,8 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-  protected
-
-  def after_sign_up_path_for(resource)
-    raise
-    '/an/example/path' # Or :prefix_to_your_route
-  end
-end
